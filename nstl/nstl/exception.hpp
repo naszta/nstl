b93922b3b2ae806dcaa3e310d637f058c1afabb4
Nasztanovics, Ferenc <naszta@naszta.hu>
2026-07-30 17:12:00 +0100

    Nested exception macro added

--- nstl/nstl/exception.hpp
+++ nstl/nstl/exception.hpp
@@ -45,4 +45,14 @@ std::ostream& operator<<(std::ostream& os_, const exception& exc_);
 
 #define NSTL2_THROW_EXCEPTION_IF(cond, detail) NSTL2_THROW_EXCEPTION_IF_IMPL(cond, ::nstl::exception, detail)
 
+#define NSTL2_NESTED_THROW_EXCEPTION_IMPL(error, detail)                                 \
+    do                                                                                   \
+    {                                                                                    \
+        std::ostringstream _oss__;                                                       \
+        _oss__ << ::nstl::safe_basename(__FILE__) << ':' << __LINE__ << " - " << detail; \
+        std::throw_with_nested(::nstl::exception{_oss__.str(), __FILE__, __LINE__});     \
+    } while (false)
+
+#define NSTL2_NESTED_THROW_EXCEPTION(detail) NSTL2_NESTED_THROW_EXCEPTION_IMPL(::nstl::exception, detail)
+
 #endif

071b5b25507e18c6999d363b29654e470b2de020
Nasztanovics, Ferenc <naszta@naszta.hu>
2026-07-31 14:01:45 +0100

    Logging using safe_basename_view

--- nstl/nstl/exception.hpp
+++ nstl/nstl/exception.hpp
@@ -27,12 +27,12 @@ private:
 std::ostream& operator<<(std::ostream& os_, const exception& exc_);
 } // namespace nstl
 
-#define NSTL2_THROW_EXCEPTION_IMPL(error, detail)                                        \
-    do                                                                                   \
-    {                                                                                    \
-        std::ostringstream _oss__;                                                       \
-        _oss__ << ::nstl::safe_basename(__FILE__) << ':' << __LINE__ << " - " << detail; \
-        throw ::nstl::exception{ _oss__.str(), __FILE__, __LINE__ };                     \
+#define NSTL2_THROW_EXCEPTION_IMPL(error, detail)                                             \
+    do                                                                                        \
+    {                                                                                         \
+        std::ostringstream _oss__;                                                            \
+        _oss__ << ::nstl::safe_basename_view(__FILE__) << ':' << __LINE__ << " - " << detail; \
+        throw ::nstl::exception{ _oss__.str(), __FILE__, __LINE__ };                          \
     } while (false)
 
 #define NSTL2_THROW_EXCEPTION(detail) NSTL2_THROW_EXCEPTION_IMPL(::nstl::exception, detail)
@@ -45,12 +45,12 @@ std::ostream& operator<<(std::ostream& os_, const exception& exc_);
 
 #define NSTL2_THROW_EXCEPTION_IF(cond, detail) NSTL2_THROW_EXCEPTION_IF_IMPL(cond, ::nstl::exception, detail)
 
-#define NSTL2_NESTED_THROW_EXCEPTION_IMPL(error, detail)                                 \
-    do                                                                                   \
-    {                                                                                    \
-        std::ostringstream _oss__;                                                       \
-        _oss__ << ::nstl::safe_basename(__FILE__) << ':' << __LINE__ << " - " << detail; \
-        std::throw_with_nested(::nstl::exception{ _oss__.str(), __FILE__, __LINE__ });   \
+#define NSTL2_NESTED_THROW_EXCEPTION_IMPL(error, detail)                                      \
+    do                                                                                        \
+    {                                                                                         \
+        std::ostringstream _oss__;                                                            \
+        _oss__ << ::nstl::safe_basename_view(__FILE__) << ':' << __LINE__ << " - " << detail; \
+        std::throw_with_nested(::nstl::exception{ _oss__.str(), __FILE__, __LINE__ });        \
     } while (false)
 
 #define NSTL2_NESTED_THROW_EXCEPTION(detail) NSTL2_NESTED_THROW_EXCEPTION_IMPL(::nstl::exception, detail)

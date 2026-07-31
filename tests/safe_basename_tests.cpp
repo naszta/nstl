354d5f28355b172e09710f8bfac6294ded2b0244
Nasztanovics, Ferenc <naszta@naszta.hu>
2026-07-31 13:51:54 +0100

    safe_basename_view added

--- tests/safe_basename_tests.cpp
+++ tests/safe_basename_tests.cpp
@@ -10,3 +10,13 @@ TEST(SafeBaseName, Tests)
     EXPECT_EQ(std::strcmp(::nstl::safe_basename("safe_basename_tests.cpp"), "safe_basename_tests.cpp"), 0);
     EXPECT_EQ(::nstl::safe_basename(nullptr), nullptr);
 }
+
+TEST(SafeBaseName, View)
+{
+    constexpr auto name = ::nstl::safe_basename_view(__FILE__);
+    static_assert(!name.empty());
+    EXPECT_EQ(name, "safe_basename_tests.cpp");
+    EXPECT_EQ(::nstl::safe_basename_view(__FILE__), "safe_basename_tests.cpp");
+    EXPECT_EQ(::nstl::safe_basename_view("safe_basename_tests.cpp"), "safe_basename_tests.cpp");
+    EXPECT_EQ(::nstl::safe_basename_view(std::string_view{}), std::string_view{});
+}

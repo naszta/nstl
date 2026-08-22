29ba95b412c53befd6ba20ce96da7d18453e626a
Nasztanovics, Ferenc <naszta@naszta.hu>
2026-08-22 22:00:32 +0100

    fenv fixed on MacOS

--- nstl/nstl/math.cpp
+++ nstl/nstl/math.cpp
@@ -5,21 +5,25 @@
 
 NSTL_FENV_ACCESS_ON
 
-#if (math_errhandling & MATH_ERREXCEPT) == 0
-#error This code needs to use floating point exceptions
-#endif
-
 namespace nstl::math
 {
 template <class Type>
     requires(floting_point<Type>)
 long long safe_llround_t(Type val_)
 {
-    std::feclearexcept(FE_INVALID);
-    const auto value = std::llround(val_);
-    NSTL_THROW_EXCEPTION_IF(std::fetestexcept(FE_INVALID), std::runtime_error,
+    if (math_errhandling & MATH_ERREXCEPT)
+    {
+        std::feclearexcept(FE_INVALID);
+        const auto value = std::llround(val_);
+        NSTL_THROW_EXCEPTION_IF(std::fetestexcept(FE_INVALID), std::runtime_error,
                             val_ << " cannot be converted to integer");
-    return value;
+        return value;
+    }
+    else if (std::isfinite(val_))
+    {
+        return std::llround(val_);
+    }
+    NSTL_THROW_EXCEPTION(std::runtime_error, val_ << " cannot be converted to integer");
 }
 
 long long safe_llround(float val_) { return safe_llround_t(val_); }

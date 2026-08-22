7607558fccb268bb0214d6e7651a84bacb12eab1
Nasztanovics, Ferenc <naszta@naszta.hu>
2026-08-22 12:06:29 +0100

    Read file changes automatically: tests added

--- nstl/nstl/file_trigger.hpp
+++ nstl/nstl/file_trigger.hpp
@@ -25,8 +25,9 @@ public:
 #endif
     using data_cb = std::function<void(std::span<char> data_)>;
 
-    static std::shared_ptr<file_trigger> factory(const std::filesystem::path& file_, data_cb cb_, bool sow_ = false);
-    static std::shared_ptr<file_trigger> factory(native_handle handle_, data_cb cb_, bool sow_ = false);
+    static std::shared_ptr<file_trigger> factory(const std::filesystem::path& file_, data_cb cb_, bool sow_ = false, size_t buffer_size_ = 1000000);
+    static std::shared_ptr<file_trigger> factory(native_handle handle_, data_cb cb_, bool sow_ = false,
+                                                 size_t buffer_size_ = 1000000);
 
     file_trigger(const file_trigger&) = delete;
     file_trigger& operator=(const file_trigger&) = delete;
@@ -34,6 +35,8 @@ public:
     virtual ~file_trigger() = 0;
     virtual void stop() = 0;
 };
+
+nstl::file_trigger::native_handle open_native(const std::filesystem::path& path_);
 }
 
 #endif
